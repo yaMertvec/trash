06d4ec440e7b97f8419e9e133cae634b3145bb1c
yaMertvec <yamertvec@mail.ru>
2022-04-15 05:04:23 +0300

    всякий мусор

---
+++ app.js
@@ -0,0 +1,82 @@
+// const cars = ["mazda", "ford", "bmw"]
+// const fib = [1,2,3,5,8,13]
+//
+//
+// cars.push("пазик") // добавить в конец массива
+// console.log(cars)
+// cars.unshift("жижа") // добавить в начало массива
+// сars.shift() // удаляет из начала массива
+// сars.pop
+
+// const multiply = (a, b) => a * b;
+// console.log(multiply(2,4))
+// let string = 'I am  the code cheburek vasya';
+//
+// console.log(
+//     string.split(/\s+/).map(word => word[0].toUpperCase() + word.substring(1)).join(' ')
+// );
+// const myNoun = "dog";
+// const myAdjective = "big";
+// const myVerb = "ran";
+// const myAdverb = "quickly";
+//
+
+// const wordBlanks = myAdjective + " " + myNoun + " " + myVerb  + myAdverb +  ""; // Change this line
+// console.log(wordBlanks)
+
+// function functionWithArgs(a,b) {return a + b}
+// console.log(functionWithArgs(1,2))
+// console.log(functionWithArgs(7,9))
+
+// const people = [
+//     {name: 'Cheburek',budget: 4200},
+//     {name: 'Pelmen',budget: 3500},
+//     {name: 'Sonya',budget: 1700}
+//     ]
+// // const index = people.findIndex(function (person){
+// //     return person.budget === 3500
+// // })
+// // console.log(index)
+// let findedPerson
+// for (const person of people) {
+//     if(person.budget === 3500) {
+//         findedPerson = person
+//     }
+// }
+// console.log(findedPerson)
+// function sum(a,b) {
+//      sum = (a + b)
+//
+//     if(sum > 3) {
+//         console.log("больше 3")
+//     } else {
+//         console.log('меньше 3')
+//     }
+//     return (a+b)
+// }
+//  console.log(sum(1,2))
+
+
+// let sumAB = (a,b) => a + b
+// console.log(sumAB(1,3))
+//  let cheburek = (text,name) => text + ' ' + name
+// console.log(cheburek("привет",'бычий хуй'))
+// function showChas(num) {
+//     console.log(num)
+//     setInterval(showChas,1000,++num)
+// }
+// console.log(showChas(0))
+// function showName() {
+//     console.log("vasya")
+// }
+// setTimeout(showName,0)
+// console.log("kolya")
+// let processed = 0;
+//
+// function processArg(num) {
+//     return (num + 3) / 5;
+// }
+// console.log(processArg(7))
+// let showMessage = function (showMessage) {
+//     console.log("perdun")
+// }
